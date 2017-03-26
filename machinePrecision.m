36abc15baf53b731b4628971ed82ed84a5e78887
kbednars <krzy.bednarski97@gmail.com>
2017-03-26 17:38:51 +0200

    Delete machinePrecision.m

--- machinePrecision.m
+++
@@ -1,9 +0,0 @@
-function [eps] = machineprecision() %Obliczanie dokladnosci maszynowej komputera
-    x = 1.0;
-    rd = 1.0 + x;
-    while (rd > 1.0)
-        eps = x;        %Ostatni x spelniajacy warunek petli, zapamietujemy w zmiennej eps
-        x = x/2;
-        rd = 1.0 + x;   %Jesli rd bedzie równe 1, oznacza to, ze ostani zapamietany x to dokladnosc maszynowa
-    end
-end
